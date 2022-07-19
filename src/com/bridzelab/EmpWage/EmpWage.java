d17d77f237e199fcecee9113cb85f192bb2d81f8
Praneeth Maldakal <vengalreddysushmitha@gmail.com>
2022-07-19 18:35:06 +0530

    added working hours reached for a month

--- src/com/bridzelab/EmpWage/EmpWage.java
+++ src/com/bridzelab/EmpWage/EmpWage.java
@@ -2,22 +2,25 @@ package com.bridzelab.EmpWage;
 
 public class EmpWage {
 
-    public static final int IS_PART_TIME = 1;
+	public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
     public static final int NUM_OF_WORKING_DAYS = 2;
+    public static final int MAX_HRS_IN_MONTH = 10;
 
     public static void main(String args[]) {
 
        // Variables
 
        int empHrs = 0;
-       int empWage = 0;
-       int totalEmpWage = 0;
+       int totalEmpHrs = 0;
+       int totalWorkingDays = 0;
 
        //Computation
-       for  (int day = 0; day < NUM_OF_WORKING_DAYS; day++) 
+       while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
+               totalWorkingDays < NUM_OF_WORKING_DAYS)
         {
+             totalWorkingDays++;
              int empCheck = (int) Math.floor(Math.random() * 10) % 3;
              switch (empCheck) {
 
@@ -33,9 +36,15 @@ public class EmpWage {
 
              }
 
-             empWage = empHrs * EMP_RATE_PER_HOUR;
-             totalEmpWage += empWage;
-             System.out.println("Emp Wage:" + empWage);
+             totalEmpHrs += empHrs;
+             System.out.println("Day: " + totalWorkingDays + " Emp Hr: " + empHrs);
+
         }
-     }
- }
+
+        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Total Emp Wage: " + totalEmpWage);
+    }
+}
+
+ 
+             
