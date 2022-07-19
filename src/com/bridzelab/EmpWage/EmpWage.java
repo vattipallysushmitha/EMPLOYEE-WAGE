93f2bf951ee8cd7002dff607c8603ebc0be83b3e
Praneeth Maldakal <vengalreddysushmitha@gmail.com>
2022-07-19 17:12:09 +0530

    added employee wage part time

--- src/com/bridzelab/EmpWage/EmpWage.java
+++ src/com/bridzelab/EmpWage/EmpWage.java
@@ -3,22 +3,25 @@ package com.bridzelab.EmpWage;
 public class EmpWage {
 
 	public static void main(String[] args) {
-	
-		 int IS_FULL_TIME = 1;
-	       int EMP_RATE_PER_HOUR = 20;
+		    	       // Constants
+		    	       int IS_PART_TIME = 1;
+		    	       int IS_FULL_TIME = 1;
+		    	       int EMP_RATE_PER_HOUR = 20;
 
-	       // Variables
+		    	       // Variables
 
-	       int empHrs = 0;
-	       int empWage = 0;
+		    	       int empHrs = 0;
+		    	       int empWage = 0;
 
-	       //Computation
-	       double empCheck = Math.floor(Math.random() * 10) % 2;
-	       if (empCheck == IS_FULL_TIME)
-	           empHrs = 8;
-	       else
-	           empHrs = 0;
-	       empWage = empHrs * EMP_RATE_PER_HOUR;
-	       System.out.println("Emp Wage:" + empWage);
-	     }
-	 }
+		    	       //Computation
+		    	       double empCheck = Math.floor(Math.random() * 10) % 2;
+		    	       if (empCheck == IS_FULL_TIME)
+		    	           empHrs = 4;
+		    	       else if (empCheck == IS_FULL_TIME)
+		    	           empHrs = 8;
+		    	       else
+		    	           empHrs = 0;
+		    	       empWage = empHrs * EMP_RATE_PER_HOUR;
+		    	       System.out.println("Emp Wage:" + empWage);
+		    	     }
+		    	 }
