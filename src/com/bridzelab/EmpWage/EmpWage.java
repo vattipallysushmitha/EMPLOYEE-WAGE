bcc4a971c3361c2cd6d869ec795da677c6576c5b
Praneeth Maldakal <vengalreddysushmitha@gmail.com>
2022-08-10 22:12:13 +0530

    ADDED[sushmitha]added computing the employee wage for multiple companies

--- src/com/bridzelab/EmpWage/EmpWage.java
+++ src/com/bridzelab/EmpWage/EmpWage.java
@@ -1,13 +1,14 @@
 package com.bridzelab.EmpWage;
 
-public class EmpWage {
-	    public static final int IS_PART_TIME = 1;
-	    public static final int IS_FULL_TIME = 2;
-	    public static final int EMP_RATE_PER_HOUR = 20;
-	    public static final int NUM_OF_WORKING_DAYS = 2;
-	    public static final int MAX_HRS_IN_MONTH = 10;
+import java.util.Random;
 
-	    public static int computeEmpWage(){
+public class EmpWage 
+{
+	 public static final int IS_PART_TIME = 1;
+	    public static final int IS_FULL_TIME = 2;
+
+	    public static int computeEmpWage(String company, int empRatePerHour,
+	                                     int  numOfWorkingDays, int maxHoursPerMonth){
 
 	       // Variables
 
@@ -16,8 +17,8 @@ public class EmpWage {
 	       int totalWorkingDays = 0;
 
 	       //Computation
-	       while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
-	               totalWorkingDays < NUM_OF_WORKING_DAYS)
+	       while (totalEmpHrs <=maxHoursPerMonth &&
+	               totalWorkingDays < numOfWorkingDays)
 	        {
 	             totalWorkingDays++;
 	             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
@@ -40,12 +41,23 @@ public class EmpWage {
 
 	        }
 
-	        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-	        System.out.println("Total Emp Wage: " + totalEmpWage);
+	        int totalEmpWage = totalEmpHrs * empRatePerHour;
+	        System.out.println("Total Emp Wage for Company: " +
+	                            company+" is: " + totalEmpWage);
 	        return totalEmpWage;
 	     }
 
-	     public static void main(String[] args) {
-	        computeEmpWage();
+	     public static void main(String[] args) 
+	     {
+	    	 System.out.println("Welcome to Employee Wage for multiple companies");
+
+	    	 System.out.println("*********************************");
+	        computeEmpWage("ORACLE", 20, 2, 10);
+	        System.out.println("*********************************");
+	        computeEmpWage("AMAZON", 10, 4, 20);
+	        System.out.println("*********************************");
+	        computeEmpWage("IBM", 50, 30, 200);
+	        System.out.println("*********************************");
+	        computeEmpWage("BRIDZELAB", 50, 3, 50);
 	     }
 	}
